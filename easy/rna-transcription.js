90dad0819b708c43359743b6eaf7cc56870592c1
Alex Curran <alexcurran.va@gmail.com>
2026-09-24 10:54:38 -0500

    Solution 1: hash map linear time

---
+++ easy/rna-transcription.js
@@ -0,0 +1,28 @@
+/**
+ * solution 1 -- hash map
+ * time: O(n)
+ * space: O(1) -- not including results string
+ *
+ * @param {string} dnaChars
+ * @returns {string}
+ */
+export function toRna(dnaChars) {
+    // map DNA to RNA
+    const dnaToRnaMap = new Map([
+        ["G", "C"],
+        ["C", "G"],
+        ["T", "A"],
+        ["A", "U"],
+    ]);
+
+    let result = "";
+
+    for (let dnaChar of dnaChars) {
+        const convertedRnaChar = dnaToRnaMap.get(dnaChar) || "";
+        result += convertedRnaChar;
+    }
+
+    return result;
+}
+
+console.log(toRna("ACGTGGTCTTAA")); // "UGCACCAGAAUU"
